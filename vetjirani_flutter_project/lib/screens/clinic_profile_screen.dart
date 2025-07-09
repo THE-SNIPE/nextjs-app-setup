eff33315a0c7dda41da2a3f28690719f08a98f54
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:37 +0300

    Add vetjirani_flutter_project/lib/screens/clinic_profile_screen.dart 1752047796621

---
+++ vetjirani_flutter_project/lib/screens/clinic_profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ClinicProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clinic Profile'),
+        backgroundColor: Color(0xFF162013),
+      ),
+      body: Center(
+        child: Text(
+          'Clinic Profile Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
