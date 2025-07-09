d0fadc49739642da374cdfc433cabe83c2649a2f
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:30 +0300

    Add vetjirani_flutter_project/lib/screens/knowledge_hub_screen.dart 1752047789763

---
+++ vetjirani_flutter_project/lib/screens/knowledge_hub_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class KnowledgeHubScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Knowledge Hub'),
+        backgroundColor: Color(0xFF171F14),
+      ),
+      body: Center(
+        child: Text(
+          'Knowledge Hub Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
