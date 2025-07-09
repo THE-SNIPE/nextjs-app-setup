fd4c35beead82ec2cbad1d36b367eb8390c7846b
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:26 +0300

    Add vetjirani_flutter_project/lib/screens/vet_profile_management_screen.dart 1752047786261

---
+++ vetjirani_flutter_project/lib/screens/vet_profile_management_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VetProfileManagementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vet Profile Management'),
+        backgroundColor: Color(0xFF162013),
+      ),
+      body: Center(
+        child: Text(
+          'Vet Profile Management Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
