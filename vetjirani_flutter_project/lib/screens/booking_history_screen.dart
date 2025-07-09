60f55af9c8c398c5e2d794d599d0950ebb999b82
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:33 +0300

    Add vetjirani_flutter_project/lib/screens/booking_history_screen.dart 1752047793122

---
+++ vetjirani_flutter_project/lib/screens/booking_history_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BookingHistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Booking History'),
+        backgroundColor: Color(0xFF171F14),
+      ),
+      body: Center(
+        child: Text(
+          'Booking Confirmation / History Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
