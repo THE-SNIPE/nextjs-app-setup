9169d995b5c424f4b2985b8d4561348b3ce3d4bd
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:28 +0300

    Add vetjirani_flutter_project/lib/screens/farmer_profile_settings_screen.dart 1752047788062

---
+++ vetjirani_flutter_project/lib/screens/farmer_profile_settings_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FarmerProfileSettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile & Settings'),
+        backgroundColor: Color(0xFF171F14),
+      ),
+      body: Center(
+        child: Text(
+          'Farmer Profile & Settings Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
