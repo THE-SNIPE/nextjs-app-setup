06dcf83abee0d9ce9bee192ab604e25024873d39
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-09 10:56:24 +0300

    Add vetjirani_flutter_project/lib/screens/vet_reviews_screen.dart 1752047783539

---
+++ vetjirani_flutter_project/lib/screens/vet_reviews_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VetReviewsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vet Reviews & Ratings'),
+        backgroundColor: Color(0xFF171F14),
+      ),
+      body: Center(
+        child: Text(
+          'Vet Reviews & Ratings Screen - To be implemented',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
